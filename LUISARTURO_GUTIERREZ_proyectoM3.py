075fceb62fdb852dd19abb8d72b22328238a9ab7
Shynomni <107893520+Shynomni@users.noreply.github.com>
2023-06-03 19:52:53 -0700

    ProyectoM3.py
    Se crea un programa para simular la máquina de Galton y generar su  histograma.  Se instalan bibliotecas utilizando el comando pip install en  Python.   Se utiliza la función random.choice() de la biblioteca random para elegir al azar entre las opciones "Izquierda" y "Derecha".   La función simular_galton() se encarga de simular la máquina de Galton y devuelve una lista con la cantidad de canicas en cada contenedor. La función graficar_histograma() toma esa lista y genera un histograma utilizando la biblioteca matplotlib.pyplot.  El contador del contenedor correspondiente se incrementa correctamente según el resultado de la elección aleatoria. La función graficar_histograma() utiliza la biblioteca matplotlib.pyplot para generar un histograma de los datos proporcionados

---
+++ LUISARTURO_GUTIERREZ_proyectoM3.py
@@ -0,0 +1,35 @@
+import random
+import matplotlib.pyplot as plt
+
+def simular_galton(num_canicas, num_niveles):
+    contenedores = [0] * (num_niveles + 1)  # Inicializar los contenedores con 0 canicas
+
+    for _ in range(num_canicas):
+        nivel = 0  # Comenzar en el nivel superior
+        for _ in range(num_niveles):
+            lado = random.choice(['Izquierda', 'Derecha'])  # Elegir al azar izquierda o derecha
+            if lado == 'Derecha':
+                nivel += 1  # Avanzar al siguiente nivel a la derecha
+        contenedores[nivel] += 1  # Incrementar el contador del contenedor correspondiente
+
+    return contenedores
+
+def graficar_histograma(contenedores):
+    num_contenedores = len(contenedores)
+    niveles = list(range(num_contenedores))
+    cantidad_canicas = contenedores
+
+    plt.bar(niveles, cantidad_canicas)
+    plt.xlabel('Contenedor')
+    plt.ylabel('Cantidad de canicas')
+    plt.title('Máquina de Galton - Histograma de canicas en cada contenedor')
+    plt.show()
+
+# Simular la máquina de Galton con 3000 canicas y 12 niveles
+num_canicas = 3000
+num_niveles = 12
+
+contenedores = simular_galton(num_canicas, num_niveles)
+graficar_histograma(contenedores)
+
+
